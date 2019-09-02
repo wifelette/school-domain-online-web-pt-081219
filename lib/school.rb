2e806991b3e5ebbb3795ffa5485e685e49cf22e8
Leah Silber <leahsilber@gmail.com>
2019-09-02 06:02:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade].has_key?(grade)
+    if @roster.has_key?(grade)
       @roster[grade] << name
     else
       @roster[grade] = []
