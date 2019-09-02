b5c13486d0f355fe160b106e8a5bcafa1f79869f
Leah Silber <leahsilber@gmail.com>
2019-09-02 06:13:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader
+  attr_writer :name, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_pair do |grade, students|
+      students.sort!
+    end
+  end
+
+end
